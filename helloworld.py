ddfca104cc1af9c8b753bdc8a0604ffd75235c0d
Sujan Kumar Patro <sujanpatro@gmail.com>
2020-05-07 20:44:07 +0530

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+first_name = 'SUJAN'
+last_name = 'PATRO'
+
+#output = 'Hello, ' + first_name + ' ' + last_name
+# output = 'Hello, {} {}'.format(first_name, last_name)
+#output = 'Hello, {1}, {0}'.format(first_name, last_name)
+output = f'Hello, {first_name} {last_name}'
+print(output)
